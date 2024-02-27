69d3a0ed649e0032a4658d36e04292e9714e2bd5
codebudy5247 <a.shekhar5247@gmail.com>
2024-02-27 15:41:39 +0530

    update navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) {
+    redirect(`/trip`);
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <h1 className="text-7xl underline">A Social trip planner.</h1>
+      <h1 className="text-7xl underline">A Social trip planner.</h1>
     </main>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,32 @@
+"use client";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Logo } from "./Logo";
-export const Navbar = () => {
+import { signOut } from "next-auth/react";
+import { User } from "@prisma/client";
+
+type Props = {
+  user: User;
+};
+
+export const Navbar = ({ user }: Props) => {
   return (
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="outline" asChild>
-            <Link href="/signin">Login</Link>
-          </Button>
-          <Button size="sm" asChild>
-            <Link href="/signup">Get started</Link>
-          </Button>
+          {!user && (
+            <>
+              <Button size="sm" variant="outline" asChild>
+                <Link href="/signin">Login</Link>
+              </Button>
+              <Button size="sm" asChild>
+                <Link href="/signup">Get started</Link>
+              </Button>
+            </>
+          )}
+
+          {user && <Button onClick={() => signOut()}>Logout</Button>}
         </div>
       </div>
     </div>
